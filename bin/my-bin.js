32e6c7a1797f7954f43322879417c2180449193a
TZ <atian25@qq.com>
2017-09-06 14:56:57 +0800

    feat: cfork

--- bin/my-bin.js
+++ bin/my-bin.js
@@ -5,9 +5,21 @@
 const path = require('path');
 const fork = require('child_process').fork;
 const proxy = require('../proxy');
+const cfork = require('cfork');
 
 console.log('log at start, parent process');
 
 const bin = path.join(__dirname, '../index.js');
-fork(bin, [], { execArgv: [ '--inspect=9999' ] });
+// fork(bin, [], { execArgv: [ '--inspect=9999' ] });
+
+
+cfork({
+  exec: bin,
+  args: [],
+  execArgv: [ '--inspect=9999' ],
+  silent: false,
+  count: 1,
+  refork: true,
+});
+
 proxy();
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 'use strict';
 
-console.log('this is child process');
+const http = require('http');
 
-let i = 0;
-setInterval(() => {
-  console.log(i++);
-}, 3000);
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+
+  if (req.url === '/exit') {
+    const msg = `exit: ${process.pid}\n${process.debugPort}`;
+    console.log(msg);
+    res.end(msg);
+    process.exit();
+  } else {
+    res.end(`hello world\n${process.pid}\n${process.debugPort}`);
+  }
+}).listen(7001);
+
+console.log('server#%s: 127.0.0.1:7001, debugPort: %s', process.pid, process.debugPort);
